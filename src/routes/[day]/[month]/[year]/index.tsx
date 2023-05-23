81895827c6337bf7db0b8554fef1a530539fd8c1
alfredskeden <alfred.skedeback91@gmail.com>
2023-05-23 10:57:30 +0200

    fix reset of signal when redirecting

--- src/routes/[day]/[month]/[year]/index.tsx
+++ src/routes/[day]/[month]/[year]/index.tsx
@@ -1,126 +1,135 @@
-import { component$, useSignal } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
-import { routeLoader$ } from '@builder.io/qwik-city';
-import type { SimilarData } from '@prisma/client';
-import dayjs from 'dayjs';
-import PreviousAndNextDayLinks from '~/components/PreviousAndNextDayLinks';
-import VideoPlayer from '~/components/VideoPlayer';
-import WordOfTheDayDescription from '~/components/WordOfTheDayDescription';
-import WordOfTheDayTitle from '~/components/WordOfTheDayTitle';
-import WordSelectionButtons from '~/components/WordSelectionButtons';
-import type { TSPQuizResponse } from '~/types/tspquiz';
-import { futureData } from '~/utils/futureData';
-import { prisma } from '~/utils/prisma';
+import { component$, useSignal } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { useLocation } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import type { SimilarData } from "@prisma/client";
+import dayjs from "dayjs";
+import PreviousAndNextDayLinks from "~/components/PreviousAndNextDayLinks";
+import VideoPlayer from "~/components/VideoPlayer";
+import WordOfTheDayDescription from "~/components/WordOfTheDayDescription";
+import WordOfTheDayTitle from "~/components/WordOfTheDayTitle";
+import WordSelectionButtons from "~/components/WordSelectionButtons";
+import type { TSPQuizResponse } from "~/types/tspquiz";
+import { futureData } from "~/utils/futureData";
+import { prisma } from "~/utils/prisma";
 
-export const apiString = 'https://tspquiz.se/api/';
+export const apiString = "https://tspquiz.se/api/";
 
 export const useTodaysWord = routeLoader$(async (requestEvent) => {
-  const dateCreated = `${requestEvent.params.day}/${requestEvent.params.month}/${requestEvent.params.year}`;
-  if (
-    dayjs(
-      `${requestEvent.params.year}-${requestEvent.params.month}-${requestEvent.params.day}`
-    ).diff(dayjs()) > 0
-  ) {
-    return [{ ...futureData }];
-  }
+ const dateCreated = `${requestEvent.params.day}/${requestEvent.params.month}/${requestEvent.params.year}`;
+ if (
+  dayjs(
+   `${requestEvent.params.year}-${requestEvent.params.month}-${requestEvent.params.day}`
+  ).diff(dayjs()) > 0
+ ) {
+  return [{ ...futureData }];
+ }
 
-  const word = await prisma.wordOfTheDay.findFirst({
-    where: {
-      dateCreated,
-    },
-    include: {
-      similarDataId: true,
-    },
+ const word = await prisma.wordOfTheDay.findFirst({
+  where: {
+   dateCreated,
+  },
+  include: {
+   similarDataId: true,
+  },
+ });
+
+ if (!word) {
+  const res1 = await fetch(`${apiString}?action=random&count=1`);
+  const randomWord: TSPQuizResponse[] = await res1.json();
+
+  const { id, word, description, movie, movie_image } = randomWord[0];
+
+  const createdWord = await prisma.wordOfTheDay.create({
+   data: {
+    dateCreated,
+    word,
+    description,
+    movie,
+    movie_image,
+    signId: id,
+   },
   });
 
-  if (!word) {
-    const res1 = await fetch(`${apiString}?action=random&count=1`);
-    const randomWord: TSPQuizResponse[] = await res1.json();
+  const res2 = await fetch(
+   `${apiString}?action=all-by-word&word=${encodeURI(
+    createdWord.word
+   )}&flexible_match=1&max_count=30&excludeUncommon=0`
+  );
+  const similarWords: TSPQuizResponse[] = await res2.json();
 
-    const { id, word, description, movie, movie_image } = randomWord[0];
+  const similarData: SimilarData[] = [];
 
-    const createdWord = await prisma.wordOfTheDay.create({
-      data: {
-        dateCreated,
-        word,
-        description,
-        movie,
-        movie_image,
-        signId: id,
-      },
+  for (const words of similarWords) {
+   if (words.word === createdWord.word) {
+    const newSimilarWords = await prisma.similarData.create({
+     data: {
+      word: words.word,
+      description: words.description,
+      movie: words.movie,
+      movie_image: words.movie_image,
+      signId: words.id,
+      wordOfTheDayId: createdWord.id,
+     },
     });
-
-    const res2 = await fetch(
-      `${apiString}?action=all-by-word&word=${encodeURI(
-        createdWord.word
-      )}&flexible_match=1&max_count=30&excludeUncommon=0`
-    );
-    const similarWords: TSPQuizResponse[] = await res2.json();
-
-    const similarData: SimilarData[] = [];
-
-    for (const words of similarWords) {
-      if (words.word === createdWord.word) {
-        const newSimilarWords = await prisma.similarData.create({
-          data: {
-            word: words.word,
-            description: words.description,
-            movie: words.movie,
-            movie_image: words.movie_image,
-            signId: words.id,
-            wordOfTheDayId: createdWord.id,
-          },
-        });
-        similarData.push(newSimilarWords);
-      }
-    }
-    return similarData;
+    similarData.push(newSimilarWords);
+   }
   }
+  return similarData;
+ }
 
-  return word.similarDataId;
+ return word.similarDataId;
 });
 
 export default component$(() => {
-  const wordOfTheDay = useTodaysWord();
-  const selectedId = useSignal(0);
+ const wordOfTheDay = useTodaysWord();
+ const selectedId = useSignal(0);
+ const loc = useLocation();
 
-  if (wordOfTheDay.value.length <= 0)
-    return (
-      <h2 class="text-3xl">
-        <Link class="underline hover:no-underline" href="/">
-          Try again
-        </Link>
-      </h2>
-    );
+ if (loc.isNavigating) selectedId.value = 0;
 
+ if (wordOfTheDay.value.length <= 0)
   return (
-    <div class="flex flex-col justify-center items-center gap-4 px-4 md:p-0">
-      <div class="flex flex-col justify-center items-center gap-4 px-4 md:p-0">
-        <PreviousAndNextDayLinks />
-        <WordOfTheDayTitle
-          word={wordOfTheDay.value[selectedId.value].word}
-          signId={wordOfTheDay.value[selectedId.value].signId}
-        />
-        <VideoPlayer
-          movie={wordOfTheDay.value[selectedId.value].movie}
-          movieImage={wordOfTheDay.value[selectedId.value].movie_image}
-        />
-        {wordOfTheDay.value.length > 1 && (
-          <WordSelectionButtons wordOfTheDay={wordOfTheDay.value} selectedId={selectedId} />
-        )}
-        <WordOfTheDayDescription description={wordOfTheDay.value[selectedId.value].description} />
-      </div>
-    </div>
+   <h2 class="text-3xl">
+    <Link class="underline hover:no-underline" href="/">
+     Try again
+    </Link>
+   </h2>
   );
+
+ return (
+  <div class="flex flex-col justify-center items-center gap-4 px-4 md:p-0">
+   <div class="flex flex-col justify-center items-center gap-4 px-4 md:p-0">
+    <PreviousAndNextDayLinks />
+    <WordOfTheDayTitle
+     word={wordOfTheDay.value[selectedId.value].word}
+     signId={wordOfTheDay.value[selectedId.value].signId}
+    />
+    <VideoPlayer
+     movie={wordOfTheDay.value[selectedId.value].movie}
+     movieImage={wordOfTheDay.value[selectedId.value].movie_image}
+    />
+    {wordOfTheDay.value.length > 1 && (
+     <WordSelectionButtons
+      wordOfTheDay={wordOfTheDay.value}
+      selectedId={selectedId}
+     />
+    )}
+    <WordOfTheDayDescription
+     description={wordOfTheDay.value[selectedId.value].description}
+    />
+   </div>
+  </div>
+ );
 });
 
 export const head: DocumentHead = {
-  title: 'Dagens Tecken',
-  meta: [
-    {
-      name: 'description',
-      content: 'Dagens teckenspråk tecken',
-    },
-  ],
+ title: "Dagens Tecken",
+ meta: [
+  {
+   name: "description",
+   content: "Dagens teckenspråk tecken",
+  },
+ ],
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,120 +1,129 @@
-import { component$, useSignal } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import { routeLoader$ } from '@builder.io/qwik-city';
-import type { SimilarData } from '@prisma/client';
-import dayjs from 'dayjs';
-import PreviousAndNextDayLinks from '~/components/PreviousAndNextDayLinks';
-import VideoPlayer from '~/components/VideoPlayer';
-import WordOfTheDayDescription from '~/components/WordOfTheDayDescription';
-import WordOfTheDayTitle from '~/components/WordOfTheDayTitle';
-import WordSelectionButtons from '~/components/WordSelectionButtons';
-import type { TSPQuizResponse } from '~/types/tspquiz';
-import { prisma } from '~/utils/prisma';
+import { component$, useSignal } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { useLocation } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import type { SimilarData } from "@prisma/client";
+import dayjs from "dayjs";
+import PreviousAndNextDayLinks from "~/components/PreviousAndNextDayLinks";
+import VideoPlayer from "~/components/VideoPlayer";
+import WordOfTheDayDescription from "~/components/WordOfTheDayDescription";
+import WordOfTheDayTitle from "~/components/WordOfTheDayTitle";
+import WordSelectionButtons from "~/components/WordSelectionButtons";
+import type { TSPQuizResponse } from "~/types/tspquiz";
+import { prisma } from "~/utils/prisma";
 
-export const dateFormat = 'DD/MM/YYYY';
-export const apiString = 'https://tspquiz.se/api/';
+export const dateFormat = "DD/MM/YYYY";
+export const apiString = "https://tspquiz.se/api/";
 
 export const useTodaysWord = routeLoader$(async () => {
-  const word = await prisma.wordOfTheDay.findFirst({
-    where: {
-      dateCreated: dayjs().format(dateFormat),
-    },
-    include: {
-      similarDataId: true,
-    },
+ const word = await prisma.wordOfTheDay.findFirst({
+  where: {
+   dateCreated: dayjs().format(dateFormat),
+  },
+  include: {
+   similarDataId: true,
+  },
+ });
+
+ if (!word) {
+  const res1 = await fetch(`${apiString}?action=random&count=1`);
+  const randomWord: TSPQuizResponse[] = await res1.json();
+
+  const { id, word, description, movie, movie_image } = randomWord[0];
+
+  const createdWord = await prisma.wordOfTheDay.create({
+   data: {
+    dateCreated: dayjs().format(dateFormat),
+    word,
+    description,
+    movie,
+    movie_image,
+    signId: id,
+   },
   });
 
-  if (!word) {
-    const res1 = await fetch(`${apiString}?action=random&count=1`);
-    const randomWord: TSPQuizResponse[] = await res1.json();
+  const res2 = await fetch(
+   `${apiString}?action=all-by-word&word=${encodeURI(
+    createdWord.word
+   )}&flexible_match=1&max_count=30&excludeUncommon=0`
+  );
+  const similarWords: TSPQuizResponse[] = await res2.json();
 
-    const { id, word, description, movie, movie_image } = randomWord[0];
+  const similarData: SimilarData[] = [];
 
-    const createdWord = await prisma.wordOfTheDay.create({
-      data: {
-        dateCreated: dayjs().format(dateFormat),
-        word,
-        description,
-        movie,
-        movie_image,
-        signId: id,
-      },
+  for (const words of similarWords) {
+   if (words.word === createdWord.word) {
+    const newSimilarWords = await prisma.similarData.create({
+     data: {
+      word: words.word,
+      description: words.description,
+      movie: words.movie,
+      movie_image: words.movie_image,
+      signId: words.id,
+      wordOfTheDayId: createdWord.id,
+     },
     });
-
-    const res2 = await fetch(
-      `${apiString}?action=all-by-word&word=${encodeURI(
-        createdWord.word
-      )}&flexible_match=1&max_count=30&excludeUncommon=0`
-    );
-    const similarWords: TSPQuizResponse[] = await res2.json();
-
-    const similarData: SimilarData[] = [];
-
-    for (const words of similarWords) {
-      if (words.word === createdWord.word) {
-        const newSimilarWords = await prisma.similarData.create({
-          data: {
-            word: words.word,
-            description: words.description,
-            movie: words.movie,
-            movie_image: words.movie_image,
-            signId: words.id,
-            wordOfTheDayId: createdWord.id,
-          },
-        });
-        similarData.push(newSimilarWords);
-      }
-    }
-    return similarData;
+    similarData.push(newSimilarWords);
+   }
   }
+  return similarData;
+ }
 
-  return word.similarDataId;
+ return word.similarDataId;
 });
 
 export default component$(() => {
-  const wordOfTheDay = useTodaysWord();
-  const selectedId = useSignal(0);
+ const wordOfTheDay = useTodaysWord();
+ const selectedId = useSignal(0);
+ const loc = useLocation();
 
-  if (wordOfTheDay.value.length <= 0) {
-    return (
-      <div class="flex justify-center h-screen mt-4">
-        <h2 class="text-3xl">Try again</h2>
-      </div>
-    );
-  }
+ if (loc.isNavigating) selectedId.value = 0;
 
+ if (wordOfTheDay.value.length <= 0) {
   return (
-    <div class="flex flex-col justify-center items-center gap-4 px-4 md:p-0">
-      <PreviousAndNextDayLinks />
-      <WordOfTheDayTitle
-        word={wordOfTheDay.value[selectedId.value].word}
-        signId={wordOfTheDay.value[selectedId.value].signId}
-      />
-      <VideoPlayer
-        movieImage={wordOfTheDay.value[selectedId.value].movie_image}
-        movie={wordOfTheDay.value[selectedId.value].movie}
-      />
-      {wordOfTheDay.value.length > 1 && (
-        <WordSelectionButtons wordOfTheDay={wordOfTheDay.value} selectedId={selectedId} />
-      )}
-      <WordOfTheDayDescription description={wordOfTheDay.value[selectedId.value].description} />
-    </div>
+   <div class="flex justify-center h-screen mt-4">
+    <h2 class="text-3xl">Try again</h2>
+   </div>
   );
+ }
+
+ return (
+  <div class="flex flex-col justify-center items-center gap-4 px-4 md:p-0">
+   <PreviousAndNextDayLinks />
+   <WordOfTheDayTitle
+    word={wordOfTheDay.value[selectedId.value].word}
+    signId={wordOfTheDay.value[selectedId.value].signId}
+   />
+   <VideoPlayer
+    movieImage={wordOfTheDay.value[selectedId.value].movie_image}
+    movie={wordOfTheDay.value[selectedId.value].movie}
+   />
+   {wordOfTheDay.value.length > 1 && (
+    <WordSelectionButtons
+     wordOfTheDay={wordOfTheDay.value}
+     selectedId={selectedId}
+    />
+   )}
+   <WordOfTheDayDescription
+    description={wordOfTheDay.value[selectedId.value].description}
+   />
+  </div>
+ );
 });
 
 export const head: DocumentHead = ({ resolveValue }) => {
-  const word = resolveValue(useTodaysWord);
-  return {
-    title: `Dagens Tecken "${word[0].word}"`,
-    meta: [
-      {
-        name: 'description',
-        content: word[0].description,
-      },
-      {
-        name: 'id',
-        content: word[0].id,
-      },
-    ],
-  };
+ const word = resolveValue(useTodaysWord);
+ return {
+  title: `Dagens Tecken "${word[0].word}"`,
+  meta: [
+   {
+    name: "description",
+    content: word[0].description,
+   },
+   {
+    name: "id",
+    content: word[0].id,
+   },
+  ],
+ };
 };
