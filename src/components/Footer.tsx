390c47ce7cb88d38a152400dc5fb098b9b26d118
Taffman <alfred.skedeback91@gmail.com>
2023-05-28 15:02:01 +0200

    fix footer classes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,14 @@ import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
-    <footer class="flex mt-auto min-h-[40px] justify-center border border-t-2">
-      <div class="flex gap-1 p-4">
-        <span>UMAIN</span> -{' '}
+    <footer class="flex mt-auto min-h-[40px] md:justify-center border border-t-2">
+      <div class="flex flex-col md:flex-row gap-4 p-4">
+        <Link href="https://umain.com/">UMAIN</Link>
         <Link class="hover:underline" href="https://github.com/alfredskeden/dagens-tecken-qwik">
           alfredskeden
-        </Link>{' '}
-        -{' '}
+        </Link>
         <span>
-          Videos av{' '}
+          Videos från{' '}
           <Link class="hover:underline" href="https://teckensprakslexikon.su.se/">
             teckenspråkslexikon
           </Link>
