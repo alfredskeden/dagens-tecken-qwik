f63f2a8718106220d298e40da181d5678d7413b5
alfredskeden <alfred.skedeback91@gmail.com>
2023-05-31 10:17:45 +0200

    install g4a tag

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { partytownVite } from "@builder.io/partytown/utils";
+import { join } from "path";
 
 export default defineConfig(() => {
   return {
-    ssr: { target: 'webworker' },
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    ssr: { target: "webworker" },
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      partytownVite({ dest: join(__dirname, "dist", "~partytown") }),
+    ],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
