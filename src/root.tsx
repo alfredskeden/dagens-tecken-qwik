e0c45343b0d24703e9974d588e232c5bbd121ac1
alfredskeden <alfred.skedeback91@gmail.com>
2023-05-31 11:44:14 +0200

    update gtm script

--- src/root.tsx
+++ src/root.tsx
@@ -20,6 +20,16 @@ export default component$(() => {
  return (
   <QwikCityProvider>
    <head>
+    {/** <!-- Google Tag Manager -->*/}
+    <script
+     type="text/partytown"
+     dangerouslySetInnerHTML="<!-- Google Tag Manager -->(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T2P8C2G');<!-- End Google Tag Manager -->"
+    />
+    {/**<!-- End Google Tag Manager -->*/}
     <meta charSet="utf-8" />
     <link rel="manifest" href="/manifest.json" />
     <QwikPartytown forward={["dataLayer.push"]} />
@@ -34,6 +44,16 @@ export default component$(() => {
     lang="sv"
     class="bg-gray-100 text-gray-800 font-sans leading-normal min-h-screen flex flex-col"
    >
+    {/**<!-- Google Tag Manager (noscript) -->*/}
+    <noscript>
+     <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-T2P8C2G"
+      height="0"
+      width="0"
+      style="display:none;visibility:hidden"
+     />
+    </noscript>
+    {/**<!-- End Google Tag Manager (noscript) -->*/}
     <RouterOutlet />
     <ServiceWorkerRegister />
    </body>
